63ef6a310cd31013c29bcd26cd012e0373d1f132
Leonel Dominguez <leonelchavez2013@hotmail.com>
2021-02-25 23:01:45 -0600

    Se arreglo problemas del mdi y de la clase usuario, usuarioDAO y conexion

--- SistemaHotelero/src/main/java/seguridad/dominio/Usuario.java
+++ SistemaHotelero/src/main/java/seguridad/dominio/Usuario.java
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
+
 package seguridad.dominio;
 
 /**
  *
  * @author Diego Vásquez
  */
-public class Usuario {
-=======
 
-package seguridad.dominio;
-
-import java.sql.Date;
 
 /**
  *
@@ -93,6 +88,5 @@ public class Usuario {
     public void setEstado_usuario(int estado_usuario) {
         this.estado_usuario = estado_usuario;
     }
->>>>>>> dd821fef4059fd76c44b000295fe43957ca13272
     
 }
